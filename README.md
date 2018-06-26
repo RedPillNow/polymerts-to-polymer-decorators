5e040f4024637e7892c78fb39d6342b7de83bd04
Keith Strickland <keith@redpillnow.com>
2018-06-26 18:04:08 -0400

    * Started some transformations, still a long way to go * Updated README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project is a tool to convert a [PolymerTS](https://github.com/nippur72/PolymerTS#observe) element into a [polymer-decorator](https://github.com/Polymer/polymer-decorators#observetargets-string) element.
 
-**NOTE**: This project is still under development and no where near ready for use.
+**NOTE**: This project is still under active development and no where near ready for use.
 
 ## Installation
 
@@ -15,6 +15,12 @@ npm install --save-dev git+https://github.com/RedPillNow/polymerts-to-polymer-de
 This project is meant to convert a Polymer 1.x PolymerTS Element into an element which is using Polmymer 2.4 (or greater) polymer-decorators instead. This project came from the need to keep an enterprise's custom elements current with the most recent stable version of Polymer.
 
 It was also decided that this tool should support more than just a conversion to polymer-decorators, but also provide documentation for a PolymerTS project via iron-component-page or convert to just plain-jane Polymer.
+
+## Assumptions
+
+We make a few assumptions while transforming PolymerTS files to use polymer-decorators. These are listed below:
+	* Declared Properties are defined before all other types of decorated items (i.e. Observer, Listener, Computed)
+	* If there is a `ready` function, it is defined before all listener decorated items
 
 ## Usage
 
@@ -46,7 +52,22 @@ toPolymerDecorators.convertToPolymerDecorators('src/**/*.ts', options);
 
 ## Options
 
-**_Coming Soon_**
+The following options are available to configure how to Transform your source files:
+
+| Option | Type | Default | Description |
+|--------|------|---------|-------------|
+|changeInline|boolean|false|Set to true to overwrite the original source file|
+|outputPath|string|./polymerTsToPolymerDecoratorsOutput/|The path where you want the converted files placed|
+|useMetadataReflection|boolean|false|Set to true to use the Metadata Reflection API|
+|conversionType|string|polymer-decorators|Currently not used|
+|targetPolymerVersion|number|2|The target version of Polymer to convert your source files to. Currently only version 2 is supported|
+|moveSinglePropertyObserversToProperty|boolean|true|If an `@observe` tag is only watching 1 property add an `observe` property to the property it's observing and remove the `@observe` decorator if true|
+|applyDeclarativeEventListenersMixin|boolean|true|If true will add the DeclarativeEventListenersMixin to the class|
+|applyGestureEventListenersMixin|boolean|false|If true will add the GestureEventListenersMixin to the class|
+|pathToBowerComponents|string|../../bower_components|Path to the bower_components directory|
+|changeComponentClassExtension|boolean|false|If true and the component class doesn't extend `Polymer.Element` the extension class will be replaced with `Polymer.Element`|
+|glob|object|{ignore:['bower_components/**/*.*','node_modules/**/*.*']|Files we should ignore|
+|compiler|object|{stripInternal:true,target:ts.ScriptTarget.ES5,experimentalDecorators:true,listEmittedFiles:true}|TypeScript Compiler options|
 
 ## Contributing
 
--- src/utils/transformer.ts
+++ src/utils/transformer.ts
@@ -1,17 +1,19 @@
 import * as ts from 'typescript';
 import * as chalk from './chalkConfig';
 import {RedPill} from 'polymerts-models';
-import {PostTransformChangeRecord, ChangeType, ConverterOptions} from './custom-types';
+import {TransformChangeRecord, TransformChangeType, ConverterOptions} from './custom-types';
 
 export class PolymerTsTransformerFactory {
 	private _sourceFile: ts.SourceFile;
 	private _targetPolymerVersion: number;
 	private _options: ConverterOptions;
+	private _transformer: PolymerTsTransformer;
 
 	constructor(sourceFile: ts.SourceFile, options: ConverterOptions, targetPolymerVersion?: number) {
 		this._sourceFile = sourceFile;
 		this._targetPolymerVersion = targetPolymerVersion;
 		this._options = options;
+		this._transformer = new PolymerTsTransformer(this._options);
 	}
 	/**
 	 * The transformer target Polymer "Major" version number. Defaults to 2.
@@ -28,9 +30,14 @@ export class PolymerTsTransformerFactory {
 		this._targetPolymerVersion = targetPolymerVersion ? targetPolymerVersion : 2;
 	}
 
-	transform(ctx: ts.TransformationContext) {
-		const transformer = new PolymerTsTransformer(ctx, this._options);
-		return transformer.transform.apply(transformer, [ctx, this._sourceFile]);
+	_preTransform(ctx: ts.TransformationContext) {
+		console.log(chalk.processing('Analyzing source file for required changes...'));
+	}
+
+	transform(ctx: ts.TransformationContext): ts.Transformer<ts.SourceFile> {
+		this._preTransform(ctx);
+		let transformer: ts.Transformer<ts.SourceFile> = this._transformer.transform.apply(this._transformer, [ctx, this._sourceFile]);
+		return transformer;
 	}
 }
 
@@ -38,11 +45,11 @@ export class PolymerTsTransformer {
 	private _ctx: ts.TransformationContext;
 	private _nodeMap: Map<ts.Node, ts.Node|null> = new Map();
 	private _sourceFile: ts.SourceFile;
-	private _changeRecords: PostTransformChangeRecord[] = [];
+	private _changeRecords: TransformChangeRecord[] = [];
 	private _options: ConverterOptions;
+	private _transformNodeMap: Map<ts.Node, TransformChangeRecord> = new Map();
 
-	constructor(ctx: ts.TransformationContext, options: ConverterOptions) {
-		this._ctx = ctx;
+	constructor(options: ConverterOptions) {
 		this._options = options;
 	}
 
@@ -85,6 +92,64 @@ export class PolymerTsTransformer {
 			hostAttributes: null
 		};
 	}
+
+	getTransformNodes(ctx: ts.TransformationContext, sf: ts.SourceFile) {
+		this._sourceFile = sf;
+		this._ctx = ctx;
+		const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
+			node = ts.visitEachChild(node, visitor, this._ctx);
+			switch (node.kind) {
+				case ts.SyntaxKind.ClassDeclaration:
+					let classDecl = ts.isClassDeclaration(node) ? node as ts.ClassDeclaration : null;
+					if (classDecl && RedPill.isComponent(classDecl, sf)) {
+						let comp = new RedPill.Component(classDecl);
+						comp.sourceFile = sf;
+						this._transformNodeMap.set(classDecl, null);
+					}
+					break;
+				case ts.SyntaxKind.ModuleDeclaration:
+					break;
+				case ts.SyntaxKind.MethodDeclaration:
+					// TODO: We need to transform this one last so we have the required changes to members?
+					let methodDeclNode = ts.isMethodDeclaration(node) ? node as ts.MethodDeclaration : null;
+					if (methodDeclNode && RedPill.isComputedProperty(methodDeclNode, sf)) {
+						let compProp = new RedPill.ComputedProperty(methodDeclNode);
+						compProp.sourceFile = sf;
+						this._transformNodeMap.set(methodDeclNode, null);
+					}else if (methodDeclNode && RedPill.isListener(methodDeclNode, sf)) {
+						let list = new RedPill.Listener(methodDeclNode);
+						list.sourceFile = sf;
+						this._transformNodeMap.set(methodDeclNode, null);
+					}else if (methodDeclNode && RedPill.isObserver(methodDeclNode, sf)) {
+						let obs = new RedPill.Observer(methodDeclNode);
+						obs.sourceFile = sf;
+						let chgRec: TransformChangeRecord = null;
+						if (obs.params && obs.params.length === 1) {
+							let propName = obs.params[0];
+							chgRec = {
+								changeType: TransformChangeType.PropertyChange,
+								origNode: this._findExistingDeclaredProperty(propName)
+							}
+						}
+						this._transformNodeMap.set(methodDeclNode, chgRec);
+					}else {
+
+					}
+					break;
+				case ts.SyntaxKind.PropertyDeclaration:
+					let propDeclNode = ts.isPropertyDeclaration ? node as ts.PropertyDeclaration : null;
+					if (propDeclNode && RedPill.isDeclaredProperty(propDeclNode)) {
+						let prop = new RedPill.Property(propDeclNode);
+						prop.sourceFile = sf;
+						this._transformNodeMap.set(propDeclNode, null);
+					}
+					break;
+				default:
+					// do nothing
+			}
+			return node;
+		}
+	}
 	/**
 	 * This starts the visitor
 	 * @param ctx {ts.TransformationContext}
@@ -92,6 +157,7 @@ export class PolymerTsTransformer {
 	 */
 	transform(ctx: ts.TransformationContext, sf: ts.SourceFile): ts.Transformer<ts.SourceFile> {
 		this._sourceFile = sf;
+		this._ctx = ctx;
 		const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
 			node = ts.visitEachChild(node, visitor, this._ctx);
 			const rpNode = this._nodeMap.get(node);
@@ -101,40 +167,41 @@ export class PolymerTsTransformer {
 					if (classDecl && RedPill.isComponent(classDecl, sf) && !rpNode) {
 						let comp = new RedPill.Component(classDecl);
 						comp.sourceFile = sf;
-						this.notifyUser(chalk.processing('Parsing the ' + comp.name + '(' + (<any>ts).SyntaxKind[node.kind] + ') Component...'));
-						// let newClassDecl = this.transformClassDecl(classDecl);
-						this._nodeMap.set(node, null);
-						// return newClassDecl;
+						this.notifyUser(chalk.processing('Transforming the ' + comp.name + '(' + (<any>ts).SyntaxKind[node.kind] + ') Component...'));
+						let newClassDecl = this.transformClassDecl(classDecl);
+						this._nodeMap.set(node, newClassDecl);
+						return newClassDecl;
 					}
 					break;
 				case ts.SyntaxKind.ModuleDeclaration:
 					break;
 				case ts.SyntaxKind.MethodDeclaration:
+					// TODO: We need to transform this one last so we have the required changes to members?
 					let methodDeclNode = ts.isMethodDeclaration(node) ? node as ts.MethodDeclaration : null;
 					if (methodDeclNode && RedPill.isComputedProperty(methodDeclNode, sf) && !rpNode) {
 						let compProp = new RedPill.ComputedProperty(methodDeclNode);
 						compProp.sourceFile = sf;
-						this.notifyUser(chalk.processing('Parsing the ' + compProp.name + '(' + (<any>ts).SyntaxKind[node.kind] + ') Computed Property...'));
+						this.notifyUser(chalk.processing('Transforming the ' + compProp.name + '(' + (<any>ts).SyntaxKind[node.kind] + ') Computed Property...'));
 						let newCompProp = this.transformComputedProp(methodDeclNode);
 						this._nodeMap.set(node, newCompProp);
 						return newCompProp;
-					// TODO: We need to transform this one last so we have the required changes to members
 					}else if (methodDeclNode && RedPill.isListener(methodDeclNode, sf) && !rpNode) {
 						let list = new RedPill.Listener(methodDeclNode);
 						list.sourceFile = sf;
-						this.notifyUser(chalk.processing('Parsing the ' + list.eventName + '(' + (<any>ts).SyntaxKind[node.kind] + ') Listener...'));
+						this.notifyUser(chalk.processing('Transforming the ' + list.eventName + '(' + (<any>ts).SyntaxKind[node.kind] + ') Listener...'));
 						let newListener = this.transformListener(methodDeclNode);
 						this._nodeMap.set(node, newListener);
 						return newListener;
 					}else if (methodDeclNode && RedPill.isObserver(methodDeclNode, sf) && !rpNode) {
 						let obs = new RedPill.Observer(methodDeclNode);
 						obs.sourceFile = sf;
-						this.notifyUser(chalk.processing('Parsing the ' + obs.methodName + '(' + (<any>ts).SyntaxKind[node.kind] + ') Observer...'));
+						this.notifyUser(chalk.processing('Transforming the ' + obs.methodName + '(' + (<any>ts).SyntaxKind[node.kind] + ') Observer...'));
 						let newObserver = this.transformObserver(methodDeclNode);
 						this._nodeMap.set(node, newObserver);
 						return newObserver;
 					}else {
-						this.notifyUser(chalk.processing('Skipping \'' + methodDeclNode.name.getText(sf) + '\': Just a plain ole function, no modification required...'));
+						this.notifyUser(chalk.processing('Recording \'' + methodDeclNode.name.getText(sf) + '\': Just a plain ole function, no modification required...'));
+						this._nodeMap.set(node, null);
 					}
 					break;
 				case ts.SyntaxKind.PropertyDeclaration:
@@ -170,8 +237,12 @@ export class PolymerTsTransformer {
 			if (decTextMatch && decTextMatch.length > 0) {
 				let args = (<ts.CallExpression> dec.expression).arguments;
 				let newIdent: ts.Identifier = ts.createIdentifier(newDecoratorText);
-				let newCallExp: ts.CallExpression = ts.createCall(newIdent, undefined, args);
-				newDecorator = ts.updateDecorator(dec, newIdent);
+				let newCallExp: ts.CallExpression = ts.createCall(
+					newIdent,
+					undefined,
+					args
+				);
+				newDecorator = ts.updateDecorator(dec, newCallExp);
 				break;
 			}
 		}
@@ -179,10 +250,43 @@ export class PolymerTsTransformer {
 	}
 
 	transformClassDecl(classDecl: ts.ClassDeclaration): ts.ClassDeclaration {
-		if (!this.nodeMap.get(classDecl)) {
+		let newClassDecl: ts.ClassDeclaration = null;
+		if (classDecl && ts.isClassDeclaration(classDecl)) {
 			let newDecorator = this.transformDecorator(classDecl, this.polymerTsDecoratorRegEx.component, 'customElement');
+			let decorators = classDecl.decorators;
+			if (newDecorator && decorators && decorators.length > 0) {
+				let behaviors: RedPill.IncludedBehavior[] = this._getComponentBehaviors(classDecl);
+				let heritages: ts.HeritageClause[] = [].concat(classDecl.heritageClauses);
+				let newHeritage: ts.HeritageClause[] = [];
+				if (decorators.length > 1) { // We have behaviors here
+					if (this.options.applyGestureEventListenersMixin && this.options.applyDeclarativeEventListenersMixin) {
+
+
+
+						let extendsExpression = ts.createExpressionWithTypeArguments(
+							/* ts.TypeNode[] */ undefined,
+							/* ts.Expression */ undefined
+						)
+						let newHeritageClause = ts.createHeritageClause(
+							ts.SyntaxKind.ExtendsKeyword,
+							/* ts.ExpressionWithTypeArguments[] */ [extendsExpression]
+						);
+					}
+				}else if (decorators.length === 1) { // No Behaviors defined
+					newHeritage = heritages;
+				}
+				newClassDecl = ts.updateClassDeclaration(
+					/* ts.ClassDeclaration */ classDecl,
+					/* ts.Decorator[] */ [newDecorator],
+					/* ts.Modifier[] */ classDecl.modifiers,
+					/* ts.Identifier */ classDecl.name,
+					/* ts.TypeParameterDeclaration */ classDecl.typeParameters,
+					/* ts.HeritageClause[] */ newHeritage,
+					/* ts.ClassElement[] */ classDecl.members
+				);
+			}
 		}
-		return classDecl;
+		return newClassDecl;
 	}
 
 	transformComputedProp(methodDecl: ts.MethodDeclaration): ts.GetAccessorDeclaration {
@@ -240,9 +344,9 @@ export class PolymerTsTransformer {
 					if (existingProp) {
 						let methodName = methodDecl.name.getText(this._sourceFile);
 						let updatedProp = this._addPropertyToPropertyDecl(existingProp, 'observer', methodName);
-						let postChangeRec: PostTransformChangeRecord = {
+						let postChangeRec: TransformChangeRecord = {
 							origNode: existingProp,
-							changeType: ChangeType.PropertyChange,
+							changeType: TransformChangeType.PropertyChange,
 							newNode: updatedProp,
 							removeDecorator: decorator
 						}
@@ -267,10 +371,23 @@ export class PolymerTsTransformer {
 	}
 
 	transformListener(methodDecl: ts.MethodDeclaration): ts.MethodDeclaration {
-		if (this.options.applyDeclarativeEventListenersMixin && methodDecl && ts.isMethodDeclaration(methodDecl)) {
-
-		}else if (!this.options.applyDeclarativeEventListenersMixin) {
-			// TODO: Need to add actual event listeners somewhere, attached() maybe?
+		if (methodDecl && ts.isMethodDeclaration(methodDecl) && methodDecl.decorators && methodDecl.decorators.length > 0) {
+			let classDecl = this._findExistingClassDeclaration();
+			if (this.options.applyDeclarativeEventListenersMixin) {
+				this._addRefNodeChangeRecord(classDecl, null, this.options.pathToBowerComponents + 'polymer-decorators/mixins/declarative-event-listeners.d.ts');
+				if (this.options.applyGestureEventListenersMixin) {
+					this._addRefNodeChangeRecord(classDecl, null, this.options.pathToBowerComponents + 'polymer-decorators/mixins/gesture-event-listeners.d.ts');
+				}
+			}else {
+				let readyMethod = this._findReadyMethod();
+				this._changeRecords.push({
+					changeType: TransformChangeType.AddListenerToReady,
+					origNode: readyMethod,
+					newNode: null,
+					listenerMethod: methodDecl,
+					createReadyMethod: readyMethod ? false : true
+				});
+			}
 		}
 		return methodDecl;
 	}
@@ -292,6 +409,29 @@ export class PolymerTsTransformer {
 			}
 		});
 		return existingProp;
+	}
+
+	_findExistingClassDeclaration() {
+		let existingClass = null;
+		this._nodeMap.forEach((val: ts.Node, key: ts.Node, map: Map<ts.Node, ts.Node>) => {
+			if (ts.isClassDeclaration(key) && RedPill.isComponent(key, this._sourceFile)) {
+				existingClass = key;
+			}
+		});
+		return existingClass;
+	}
+
+	_findReadyMethod() {
+		let readyMeth = null;
+		this._nodeMap.forEach((val: ts.Node, key: ts.Node, map: Map<ts.Node, ts.Node>) => {
+			if (ts.isMethodDeclaration(key)) {
+				let methodName = key.name.getText(this._sourceFile);
+				if (methodName === 'ready') {
+					readyMeth = key;
+				}
+			}
+		});
+		return readyMeth;
 	}
 
 	_addPropertyToPropertyDecl(property: ts.PropertyDeclaration, newPropName: string, newPropInitializer: string) {
@@ -334,5 +474,34 @@ export class PolymerTsTransformer {
 			);
 		}
 		return updatedProp;
+	}
+
+	_getComponentBehaviors(classDecl: ts.ClassDeclaration) {
+		let decorators = classDecl.decorators;
+		let behaviors: RedPill.IncludedBehavior[] = [];
+		if (decorators && decorators.length > 0) {
+			for (let i = 0; i < decorators.length; i++) {
+				let decorator = decorators[i];
+				let decText = decorator.expression.getText(this._sourceFile);
+				let decTextMatchBehavior = this.polymerTsDecoratorRegEx.behavior.exec(decText);
+				if (decTextMatchBehavior && decTextMatchBehavior.length > 0) {
+					let rpBehavior = new RedPill.IncludedBehavior();
+					rpBehavior.sourceFile = this._sourceFile;
+					rpBehavior.decorator = decorator;
+					behaviors.push(rpBehavior);
+				}
+			}
+		}
+		return behaviors;
+	}
+
+	_addRefNodeChangeRecord(origNode: ts.Node, newNode: ts.Node, refPath: string) {
+		let refNodeChg: TransformChangeRecord = {
+			changeType: TransformChangeType.AddTSReferenceTag,
+			origNode: origNode,
+			newNode: newNode,
+			refNodePath: refPath
+		};
+		this._changeRecords.push(refNodeChg);
 	}
 }
